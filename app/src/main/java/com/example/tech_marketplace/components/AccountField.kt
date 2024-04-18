de3910e5759e998ccae2781088d396fac7e81b48
Beginner-Project <boripats63@nu.ac.th>
2024-04-18 16:48:42 +0700

    feat(components): make up widget to this component.

--- app/src/main/java/com/example/tech_marketplace/components/AccountField.kt
+++ app/src/main/java/com/example/tech_marketplace/components/AccountField.kt
@@ -1,14 +1,74 @@
 package com.example.tech_marketplace.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun AccountField(
     title: String,
     placeHolderText: String,
     controller: MutableState<String>,
-    onChange: (String) -> Unit
+    keyboardType: KeyboardType? = KeyboardType.Text,
+    secure: Boolean? = false,
+    onChange: (value:String) -> Unit
 ) {
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.Start) {
+        Text(text = title, color = Color.White, fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = controller.value,
+            onValueChange = onChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    shape = RoundedCornerShape(4.dp),
+                    border = BorderStroke(1.dp, color = Color.White)
+                ),
+            placeholder = {
+                Text(
+                    text = placeHolderText,
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType!!),
+            visualTransformation = if (secure!!) PasswordVisualTransformation() else VisualTransformation.None,
+            colors = TextFieldDefaults.colors(
+                focusedPlaceholderColor = Color.White,
+                unfocusedPlaceholderColor = Color.White,
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.White
+
+            )
+        )
+
+    }
 
 }
