d7e24dc1c62de85abaaa049277ea6e0849b6b4c6
Beginner-Project <boripats63@nu.ac.th>
2024-04-18 10:07:52 +0700

    feat(views): Create routing logic with strings.

--- app/src/main/java/com/example/tech_marketplace/views/onboard/Registration.kt
+++ app/src/main/java/com/example/tech_marketplace/views/onboard/Registration.kt
@@ -9,25 +9,37 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Registration() {
-    val contentPager = remember {
-        mutableListOf<@Composable() ((PagerState) -> Unit)>(
-            { Connexion(it) }, { CreateAccount(it) }, { Login(it) }
+    val coroutine = rememberCoroutineScope()
+    val contentPagerMap = remember {
+        mutableMapOf<String, @Composable() (((route: String) -> Unit) -> Unit)>(
+            "connexion" to { Connexion(it) },
+            "create_account" to { CreateAccount(onPush = it) },
+            "login" to { Login(onPush = it) }
         )
     }
-    val pagerState = rememberPagerState(pageCount = { contentPager.size })
+    val pagerState = rememberPagerState(pageCount = { contentPagerMap.size }, initialPage = 0)
+    val onPush:(String)->Unit = {
+        coroutine.launch {
+            val dest = contentPagerMap.keys.indexOf(it)
+            pagerState.animateScrollToPage(dest)
+        }
+    }
     HorizontalPager(
         state = pagerState,
         modifier = Modifier
             .fillMaxSize()
             .padding(vertical = 56.dp, horizontal = 32.dp), userScrollEnabled = false
     ) {
-        contentPager[it](pagerState)
+        val key = contentPagerMap.keys.toList()[it]
+        contentPagerMap[key]?.invoke(onPush)
 
     }
 }
