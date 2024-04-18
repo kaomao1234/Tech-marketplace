4c94f8ca062c9155617734b843fd2e2d720bb414
Beginner-Project <boripats63@nu.ac.th>
2024-04-18 16:54:08 +0700

    feat(views): make up widget into this.

--- app/src/main/java/com/example/tech_marketplace/views/onboard/Login.kt
+++ app/src/main/java/com/example/tech_marketplace/views/onboard/Login.kt
@@ -1,11 +1,107 @@
 package com.example.tech_marketplace.views.onboard
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tech_marketplace.components.AccountField
+import com.example.tech_marketplace.ui.theme.CustomColor
+import com.example.tech_marketplace.viewmodels.OnboardViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Login(onPush:(String)->Unit){
-
+fun Login(onPush: (String) -> Unit) {
+    val viewModel: OnboardViewModel = viewModel()
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Login",
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.weight(1F))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            AccountField(
+                title = "Email",
+                placeHolderText = "Email",
+                controller = viewModel.loginEmail,
+                onChange = { value -> viewModel.onChange(value, viewModel.loginEmail) },
+                keyboardType = KeyboardType.Email
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            AccountField(
+                title = "Password",
+                placeHolderText = "Password",
+                controller = viewModel.loginPassword,
+                onChange = { value -> viewModel.onChange(value, viewModel.loginPassword) },
+                secure = true,
+                keyboardType = KeyboardType.Password
+            )
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        ElevatedButton(
+            onClick = viewModel::onLoginValidate,
+            contentPadding = PaddingValues(vertical = 16.dp),
+            shape = RoundedCornerShape(4.dp),
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.elevatedButtonColors(
+                containerColor = Color.White,
+                contentColor = CustomColor.Blue700
+            )
+        ) {
+            Text(text = "Validate", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Already have an account ? ",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = CustomColor.Yellow400
+            )
+            TextButton(
+                onClick = { onPush("connexion") }, colors = ButtonDefaults.textButtonColors(
+                    contentColor = CustomColor.Yellow400,
+                    containerColor = Color.Transparent
+                ), contentPadding = PaddingValues(
+                    0.dp
+                )
+            ) {
+                Text(
+                    text = "Sign-up",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
 }
