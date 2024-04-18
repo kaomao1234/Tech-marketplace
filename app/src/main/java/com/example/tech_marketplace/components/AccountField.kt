4cb250b24af8e168ddd757dca2641fc81d2e1bbb
Beginner-Project <boripats63@nu.ac.th>
2024-04-18 10:05:29 +0700

    feat(components): create function account field.

---
+++ app/src/main/java/com/example/tech_marketplace/components/AccountField.kt
@@ -0,0 +1,14 @@
+package com.example.tech_marketplace.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+
+@Composable
+fun AccountField(
+    title: String,
+    placeHolderText: String,
+    controller: MutableState<String>,
+    onChange: (String) -> Unit
+) {
+
+}
