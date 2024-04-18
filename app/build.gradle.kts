db8d4363ae681d55b1f5c0819045e934650173ec
Beginner-Project <boripats63@nu.ac.th>
2024-04-18 16:49:25 +0700

    chore: add koin lib.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -49,6 +52,8 @@ android {
 }
 
 dependencies {
+    implementation("io.insert-koin:koin-core")
+    implementation("io.insert-koin:koin-android:3.6.0-wasm-alpha2")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
