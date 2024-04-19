d28841c5d0dd730e7de031cf6432845f2252f856
Beginner-Project <boripats63@nu.ac.th>
2024-04-19 23:36:38 +0700

    feat: fix module and change start dest.

--- app/src/main/java/com/example/tech_marketplace/MainActivity.kt
+++ app/src/main/java/com/example/tech_marketplace/MainActivity.kt
@@ -5,44 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tech_marketplace.ui.theme.TechmarketplaceTheme
-import com.example.tech_marketplace.viewmodels.AccountViewModel
-import com.example.tech_marketplace.viewmodels.CheckOutViewModel
-import com.example.tech_marketplace.viewmodels.HomeViewModel
-import com.example.tech_marketplace.viewmodels.OnboardViewModel
-import com.example.tech_marketplace.viewmodels.SearchViewModel
+import com.example.tech_marketplace.viewmodels.viewModelModule
 import com.example.tech_marketplace.views.intro.IntroView
 import com.example.tech_marketplace.views.main.MainView
 import com.example.tech_marketplace.views.onboard.OnboardView
-import kotlinx.coroutines.launch
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.android.scope.AndroidScopeComponent
+import org.koin.androidx.scope.activityScope
+import org.koin.core.annotation.KoinInternalApi
 import org.koin.core.context.startKoin
-import org.koin.dsl.module
-import kotlin.reflect.KClass
+import org.koin.core.scope.Scope
 
-val appModule = module {
-    viewModel { OnboardViewModel() };
-    viewModel { HomeViewModel() };
-    viewModel { SearchViewModel() };
-    viewModel { CheckOutViewModel() };
-    viewModel { AccountViewModel() };
-}
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), AndroidScopeComponent {
+    override val scope: Scope by activityScope()
+
+    @OptIn(KoinInternalApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -57,7 +43,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        startKoin { appModule }
+
+        startKoin {
+            modules(viewModelModule)
+        }
+
+
     }
 }
 
@@ -65,7 +56,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "onboard") {
+    NavHost(navController = navController, startDestination = "main") {
         composable("intro") {
             IntroView(navController = navController)
         }
