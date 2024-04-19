5dc3184dd73a283dd4b6ec2bbc034180c5566352
Beginner-Project <boripats63@nu.ac.th>
2024-04-19 23:53:04 +0700

    Create ProductGrid.kt

---
+++ app/src/main/java/com/example/tech_marketplace/views/home/category_info/ProductGrid.kt
@@ -0,0 +1,9 @@
+package com.example.tech_marketplace.views.home.category_info
+
+import androidx.compose.runtime.Composable
+import com.example.tech_marketplace.models.Product
+
+@Composable
+fun ProductGrid(products:List<Product>){
+
+}
