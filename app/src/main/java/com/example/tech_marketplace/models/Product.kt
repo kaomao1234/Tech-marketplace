d71959aef1601eda65706421b25e59a4e96a532e
Beginner-Project <boripats63@nu.ac.th>
2024-04-19 23:41:20 +0700

    feat(models): create product.

---
+++ app/src/main/java/com/example/tech_marketplace/models/Product.kt
@@ -0,0 +1,6 @@
+package com.example.tech_marketplace.models
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.tech_marketplace.R
+
+data class Product (val label:String,val price:Int,val image: Int);
