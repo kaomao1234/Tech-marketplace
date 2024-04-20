c7d3c34c5aac58989baf3496dc5077138b90ebf5
Beginner-Project <boripats63@nu.ac.th>
2024-04-20 10:28:15 +0700

    feat(views): create product item.

---
+++ app/src/main/java/com/example/tech_marketplace/views/home/category_info/product_grid/ProductItem.kt
@@ -0,0 +1,107 @@
+package com.example.tech_marketplace.views.home.category_info.product_grid
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tech_marketplace.models.Product
+import com.example.tech_marketplace.ui.theme.CustomColor
+
+@Composable
+fun ProductItem(props: Product, viewState: String, onPress: () -> Unit) {
+    ElevatedButton(
+        onClick = onPress,
+        modifier = Modifier
+            .padding(end = if (viewState == "grid") 8.dp else 0.dp, bottom = 8.dp)
+            .height(if (viewState == "grid") 200.dp else 100.dp),
+        contentPadding = PaddingValues(if (viewState == "grid") 20.dp else 10.dp),
+        colors = ButtonDefaults.elevatedButtonColors(
+            contentColor = CustomColor.Blue700,
+            containerColor = Color.White
+        ),
+        shape = RoundedCornerShape(6.dp),
+        elevation = ButtonDefaults.elevatedButtonElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
+        when (viewState) {
+            "grid" -> Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Image(
+                    painter = painterResource(id = props.image),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth()
+                        .weight(1F)
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = props.label,
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "USD ${props.price}",
+                    fontSize = 12.sp,
+                )
+            }
+
+            "list" -> Row(
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Image(
+                    painter = painterResource(id = props.image),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.5F)
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                Column(
+                    modifier = Modifier.weight(1F),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = props.label,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "USD ${props.price}",
+                        fontSize = 12.sp,
+                    )
+                }
+            }
+        }
+    }
+}
