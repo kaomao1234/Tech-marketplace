e146b51b3ee70154e0995cbcb6f8b9f6b87ee3ef
Beginner-Project <boripats63@nu.ac.th>
2024-04-20 10:31:17 +0700

    feat(views): update home.

--- app/src/main/java/com/example/tech_marketplace/views/home/home/Home.kt
+++ app/src/main/java/com/example/tech_marketplace/views/home/home/Home.kt
@@ -1,11 +1,9 @@
 package com.example.tech_marketplace.views.home.home
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
@@ -16,17 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.tech_marketplace.viewmodels.HomeViewModel
-import org.koin.androidx.compose.getViewModel
-import org.koin.androidx.compose.koinViewModel
-import org.koin.compose.getKoin
-import org.koin.compose.koinInject
-import org.koin.core.parameter.parametersOf
+import com.example.tech_marketplace.views.home.home.sales_grid.SalesGrid
 
 @Composable
-fun Home(navController: NavController,viewModel: HomeViewModel) {
+fun Home(navController: NavController, viewModel: HomeViewModel) {
 
     Scaffold {
         Column(
@@ -40,12 +33,10 @@ fun Home(navController: NavController,viewModel: HomeViewModel) {
             Spacer(modifier = Modifier.height(29.dp))
             ActionButton(
                 onCategories = {
-                    viewModel.onCategories {
-                        navController.navigate("categories")
-                    }
+                    navController.navigate("categories")
                 },
-                onFavorites = viewModel::onFavorites,
-                onGifts = viewModel::onGifts, onBestSelling = viewModel::onBestSelling
+                onFavorites = {},
+                onGifts = {}, onBestSelling = {}
             )
             Spacer(modifier = Modifier.height(40.dp))
             SalesGrid(
