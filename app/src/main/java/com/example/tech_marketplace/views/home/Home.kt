cbf33af4630f514426c118cb2371324723714bd4
Beginner-Project <boripats63@nu.ac.th>
2024-04-20 13:06:09 +0700

    feat(views): change import.

--- app/src/main/java/com/example/tech_marketplace/views/home/Home.kt
+++ app/src/main/java/com/example/tech_marketplace/views/home/Home.kt
@@ -1,28 +1,20 @@
 package com.example.tech_marketplace.views.home
 
-import android.util.Log
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tech_marketplace.viewmodels.HomeViewModel
-import com.example.tech_marketplace.viewmodels.OnboardViewModel
 import com.example.tech_marketplace.views.home.categories.CategoriesHome
 import com.example.tech_marketplace.views.home.category_info.CategoryInfoHome
-import com.example.tech_marketplace.views.home.home.Home
+import com.example.tech_marketplace.views.home.home_main.HomeMain
 import org.koin.androidx.compose.koinViewModel
-import org.koin.compose.getKoin
-import org.koin.compose.koinInject
-import org.koin.core.Koin
 import org.koin.core.annotation.KoinInternalApi
-import org.koin.core.qualifier.named
 
 @OptIn(KoinInternalApi::class)
 @Composable
@@ -38,7 +30,7 @@ fun HomeView() {
                 .padding(it)
         ) {
             composable("home") {
-                Home(navController = localNavController, viewModel)
+                HomeMain(navController = localNavController, viewModel)
             }
             composable("categories") {
                 CategoriesHome(navController = localNavController, viewModel)
