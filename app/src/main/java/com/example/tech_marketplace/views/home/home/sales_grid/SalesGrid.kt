aeabc20697ba3ac27f9ecbca53077681b70dbecd
Beginner-Project <boripats63@nu.ac.th>
2024-04-20 10:31:32 +0700

    feat(views): create sales grid.

---
+++ app/src/main/java/com/example/tech_marketplace/views/home/home/sales_grid/SalesGrid.kt
@@ -0,0 +1,43 @@
+package com.example.tech_marketplace.views.home.home.sales_grid
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tech_marketplace.models.ProductSale
+import kotlin.math.round
+
+
+@Composable
+fun SalesGrid(listItem: List<ProductSale>, onProductPress: (ProductSale) -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Sales",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            userScrollEnabled = false,
+            modifier = Modifier
+                .height((round(listItem.size.toFloat() / 2F) * 260F).dp)
+                .fillMaxWidth()
+        ) {
+            items(listItem.size) {
+                val item = listItem[it]
+                SalesGridItem(model = item, onProductPress = { onProductPress(item) })
+
+            }
+        }
+    }
+}
